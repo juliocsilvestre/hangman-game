8661ea461a16fc4b240c950ddbd62ee76779dd4c
Mercia <68623425+mercietc@users.noreply.github.com>
2023-08-09 21:48:32 -0300

    criacao da funcao buscaSimples

---
+++ js/forca.js
@@ -0,0 +1,20 @@
+function buscaSimples(palavraSorteada, letraBuscada) { 
+    let indice = undefined;
+
+    for(let i = 0; i < palavraSorteada.length; i++) {
+        if(palavraSorteada[i] === letraBuscada) {
+            indice = i;
+        }
+    }
+    return indice;
+}
+
+const palavraSorteada = 'javascript';
+const letraBuscada = 'c';
+const indiceSaida = buscaSimples(vetor, valorProcurado);
+
+if(indiceSaida !== undefined) {
+    console.log(`A letra ${letraBuscada} foi encontrada no indice ${indiceSaida}.`);
+} else {
+    console.log(`A letra ${letraBuscada} não foi encontrada na palavra.`);
+}
