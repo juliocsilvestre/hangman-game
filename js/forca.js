2e37862ae343d5068e81b226f78bbabf40bd0165
Mercia <68623425+mercietc@users.noreply.github.com>
2023-08-09 22:44:11 -0300

    criando funcao exibePalavra() e ajustes

--- js/forca.js
+++ js/forca.js
@@ -1,23 +1,37 @@
-function buscaSimples(palavraSorteada, letraBuscada) { 
+function buscaLetraNaPalavraSecreta(palavraSecreta, letraBuscada) { 
     let indice = undefined;
 
-    for(let i = 0; i < palavraSorteada.length; i++) {
-        if(palavraSorteada[i] === letraBuscada) {
+    for(let i = 0; i < palavraSecreta.length; i++) {
+        if(palavraSecreta[i] === letraBuscada) {
             indice = i;
         }
     }
     return indice;
 }
 
-function verificaLetraCertaOuErrada(indiceSaida){
-    if(indiceSaida !== undefined) {
-        return console.log(`A letra ${letraBuscada} foi encontrada no indice ${indiceSaida}.`);
-    } else {
-        return console.log(`A letra ${letraBuscada} não foi encontrada na palavra.`);
-    }
+function exibePalavra(palavraSecreta){
+    let auxiliar = '_'*(palavraSecreta.length);
+    console.log(auxiliar);
+    output = auxiliar.split('');
+    return output;
 }
 
-const palavraSorteada = 'javascript';
+function verificaLetraCertaOuErrada(indiceSaida){
+    let output = exibePalavra(palavraSecreta);
+    if(indiceSaida !== undefined) {
+        output[indiceSaida] = palavraSecreta[indiceSaida];
+        // console.log(`A letra ${letraBuscada} foi encontrada no indice ${indiceSaida}.`);
+    } else {
+        output[indiceSaida] = 'X';
+        //return console.log(`A letra ${letraBuscada} não foi encontrada na palavra.`);
+    }
+    console.log(output)
+    return output;
+}
+
+
+let palavraSorteada = 'script';
+palavraSecreta = palavraSorteada.split('');
 const letraBuscada = 'c';
-const indiceSaida = buscaSimples(palavraSorteada, letraBuscada);
+const indiceSaida = buscaLetraNaPalavraSecreta(palavraSecreta, letraBuscada);
 const saida = verificaLetraCertaOuErrada(indiceSaida);
